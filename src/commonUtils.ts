91df85e4a313f31bd4e711c2b7ae18231fcdabfe
Masatomi KINO <masatomix@ki-no.org>
2020-06-10 16:46:42 +0900

    あ、commonUtils.tsは、Qiitaでもつかったexcel2firestoreにあるソースをコピー。 getHeader getValuesArray  など一部のメソッドも芋づるでコピー。
    csv2json メソッド作成。これもQiitaでもつかった自分のプロジェクト csv-sample-node からコピー

--- src/commonUtils.ts
+++ src/commonUtils.ts
@@ -1,25 +1,10 @@
-// import xPopWrapper = require('xlsx-populate-wrapper')
 
-import { getHeaders, getValuesArray } from './download'
+import fs from 'fs'
+import iconv from 'iconv-lite'
+import csv from 'csvtojson'
 
 const XlsxPopulate = require('xlsx-populate')
 
-// /**
-//  * Excelファイルを読み込み、各行をデータとして配列で返すメソッド。
-//  * @param path Excelファイルパス
-//  * @param sheet シート名
-//  * @param format_func フォーマット関数。instanceは各行データが入ってくるので、任意に整形して返せばよい
-//  */
-// export const xlsx2json = async function (path: string, sheet: string, format_func?: (instance: any) => any): Promise<Array<any>> {
-//   const workbook = new xPopWrapper(path)
-//   await workbook.init()
-
-//   const instances: Array<any> = workbook.getData(sheet)
-//   if (format_func) {
-//     return instances.map(instance => format_func(instance))
-//   }
-//   return instances
-// }
 
 /**
  * Excelファイルを読み込み、各行をデータとして配列で返すメソッド。
@@ -51,6 +36,25 @@ export const xlsx2json = async (
   return instances
 }
 
+
+/**
+ * 指定したパスのcsvファイルをロードして、JSONオブジェクトとしてparseする。
+ * 全行読み込んだら完了する Promise を返す。
+ * @param filePath
+ */
+export const csv2json = (filePath: string): Promise<Array<any>> => {
+  return new Promise((resolve, reject) => {
+    const datas: any[] = []
+
+    fs.createReadStream(filePath)
+      .pipe(iconv.decodeStream('Shift_JIS'))
+      .pipe(iconv.encodeStream('utf-8'))
+      .pipe(csv().on('data', (data) => datas.push(JSON.parse(data))))
+      .on('end', () => resolve(datas))
+  })
+}
+
+
 /**
  * Excelのシリアル値を、Dateへ変換します。
  * @param serialNumber シリアル値
@@ -64,4 +68,17 @@ export const toBoolean = function (boolStr: string | boolean): boolean {
     return boolStr
   }
   return boolStr.toLowerCase() === 'true'
+}
+
+
+// XlsxPopulate
+export const getHeaders = (workbook: any, sheetName: string): string[] => {
+  return workbook.sheet(sheetName).usedRange().value().shift()
+}
+
+// XlsxPopulate
+export const getValuesArray = (workbook: any, sheetName: string): any[][] => {
+  const valuesArray: any[][] = workbook.sheet(sheetName).usedRange().value()
+  valuesArray.shift() // 先頭除去
+  return valuesArray
 }
