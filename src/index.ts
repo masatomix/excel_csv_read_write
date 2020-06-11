b8f58a406ec808cb228e93cd406da5d2d1c5b2e0
Masatomi KINO <masatomix@ki-no.org>
2020-06-11 15:24:56 +0900

    リファクタリング

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,45 @@
 import { getAndExtract } from './utils'
-import { json2xlsx, csv2json } from './ParseSamples'
 import fs from 'fs'
+import { csv2json, internalSave2Excel, xlsx2json } from './commonUtils'
 
-getAndExtract('http://jusyo.jp/downloads/new/csv/csv_13tokyo.zip').then(() => {
-  csv2json('./13tokyo.csv')
-    .then((results: any[]) => {
-      // 郵便番号が「100-000x」のものに絞ってみた
-      const fresults = results.filter((address) => address['郵便番号'].startsWith('100-000'))
-      console.table(fresults)
-      for (const address of fresults) {
-        console.log(address)
-      }
-      json2xlsx(fresults, './13tokyo.xlsx')
-    })
-    .finally(() => {
-      if (fs.existsSync('./13tokyo.csv')) {
-        fs.unlinkSync('./13tokyo.csv')
-      } else {
-        console.log('あれ？ない')
-      }
-    })
-})
-// https://tsurutoro.com/pseudo_data/
+
+/**
+ * ネットからZIP化されたファイルをダウンロードして解凍、
+ * 取り出したCSVを読んでJSONにし、最後にExcelに書き出すサンプル
+ */
+function sample1() {
+  getAndExtract('http://jusyo.jp/downloads/new/csv/csv_13tokyo.zip').then(() => {
+    csv2json('./13tokyo.csv')
+      .then((results: any[]) => {
+        // 郵便番号が「100-000x」のものに絞ってみた
+        const fresults = results.filter((address) => address['郵便番号'].startsWith('100-000'))
+        console.table(fresults)
+        for (const address of fresults) {
+          console.log(address)
+        }
+        internalSave2Excel(fresults, './13tokyo.xlsx', '', 'Sheet1')
+      })
+      .finally(() => {
+        if (fs.existsSync('./13tokyo.csv')) {
+          fs.unlinkSync('./13tokyo.csv')
+        } else {
+          console.log('あれ？ない')
+        }
+      })
+  })
+  // https://tsurutoro.com/pseudo_data/
+}
+
+
+function sample2() {
+  const resultPromise = xlsx2json('input.xlsx')
+  resultPromise.then((results) => {
+    console.table(results)
+    internalSave2Excel(results, 'excelResults.xlsx', '', 'Sheet1').then((path) => console.log(path))
+  })
+}
+
+if (!module.parent) {
+  // sample1()
+  sample2()
+}
--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,11 @@
 import Logger from 'bunyan'
 import config from 'config'
 
-export const getLogger = function (name: string): Logger {
+export const getLogger = function(name: string): Logger {
   const conf = config as any
   if (conf['logging']) {
     const settings: Array<any> = conf['logging']
-    const targets = settings.filter((setting) => setting.name === name)
+    const targets = settings.filter(setting => setting.name === name)
 
     if (targets && targets.length > 0) {
       const logger = Logger.createLogger(targets[0])
